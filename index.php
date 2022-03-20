c71e33b4d4f2b5b2f8cc3031ff59905cc9df3c45
cloud-vs <cloud.vs@iclod.com>
2022-03-20 18:14:37 +0500

    Update Project

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once("helpers.php");
+require_once("functions.php");
+require_once("data.php");
+
+/**
+ * @var $categories переменная подключаемого файла
+ * @var $goods переменная подключаемого файла
+ */
+$page_content = include_template("main.php", [
+   "categories" => $categories,
+   "goods" => $goods
+]);
+$layout_content = include_template("layout.php", [
+   "content" => $page_content,
+   "categories" => $categories,
+   "title" => "Главная"
+]);
+
+print($layout_content);
